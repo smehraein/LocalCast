4206c0470e812a73cfe90f0d8791102496f679af
Soroush Mehraein <soroushmehraein@gmail.com>
2015-11-09 12:10:30 -0800

    Build routes controller + other server basics
    Includes first draft of SQL schema. Can start testing once
    models are up and running

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,17 +2,91 @@ var models = require('../models');
 
 module.exports = {
 
-  messages: {
+  leagues: {
     get: function (req, res) {
-      models.messages.get(function(err, results) {
-        if (err) { /* do something */ }
-        res.json(results);
-      });
+      if (req.body.leagueid) {
+        models.leagues.getOne(req.body.leagueid, function(err, results) {
+          if (err) { console.error(err); }
+          res.json(results);
+        });
+      }
+      else {
+        models.leagues.getAll(function(err, results) {
+          if (err) { console.error(err); }
+          res.json(results);
+        });
+      }
     },
     post: function (req, res) {
-      var params = [req.body.message, req.body.username, req.body.roomname];
-      models.messages.post(params, function(err, results) {
-        if (err) { /* do something */ }
+      var params = [req.body.leaguename, req.body.leaguesport];
+      models.leagues.post(params, function(err, results) {
+        if (err) { console.error(err); }
+        res.sendStatus(201);
+      });
+    }
+  },
+
+  teams: {
+    get: function (req, res) {
+      if (req.body.teamid) {
+        models.teams.getOne(req.body.teamid, function(err, results) {
+          if (err) { console.error(err); }
+          res.json(results);
+        });
+      }
+      else if (req.body.leagueid) {
+        models.teams.getFromLeague(req.body.leagueid, function(err, results) {
+          if (err) { console.error(err); }
+          res.json(results);
+        });
+      }
+      else {
+        models.teams.getAll(function(err, results) {
+          if (err) { console.error(err); }
+          res.json(results);
+        });
+      }
+    },
+    post: function (req, res) {
+      var params = [req.body.teamname, req.body.leaguename];
+      models.teams.post(params, function(err, results) {
+        if (err) { console.error(err); }
+        res.sendStatus(201);
+      });
+    }
+  },
+
+  games: {
+    get: function (req, res) {
+      if (req.body.gameid) {
+        models.games.getOne(req.body.gameid, function(err, results) {
+          if (err) { console.error(err); }
+          res.json(results);
+        });
+      }
+      else if (req.body.teamid) {
+        models.games.getFromTeam(req.body.teamid, function(err, results) {
+          if (err) { console.error(err); }
+          res.json(results);
+        });
+      }
+      else if (req.body.leagueid) {
+        models.games.getFromLeague(req.body.leagueid, function(err, results) {
+          if (err) { console.error(err); }
+          res.json(results);
+        });
+      }
+      else {
+        models.games.getAll(function(err, results) {
+          if (err) { console.error(err); }
+          res.json(results);
+        });
+      }
+    },
+    post: function (req, res) {
+      var params = [req.body.team1id, req.body.team2id, req.body.team1score, req.body.team2score];
+      models.games.post(params, function(err, results) {
+        if (err) { console.error(err); }
         res.sendStatus(201);
       });
     }
@@ -20,17 +94,25 @@ module.exports = {
 
   users: {
     get: function (req, res) {
-      models.users.get(function(err, results) {
-        if (err) { /* do something */ }
+      if (req.body.userid) {
+        models.users.getOne(req.body.userid, function(err, results) {
+          if (err) { console.error(err); }
+          res.json(results);
+        });
+      }
+      else {
+        models.users.get(function(err, results) {
+        if (err) { console.error(err); }
         res.json(results);
-      });
+        });
+      }
     },
     post: function (req, res) {
       var params = [req.body.username];
       models.users.post(params, function(err, results) {
-        if (err) { /* do something */ }
+        if (err) { console.error(err); }
         res.sendStatus(201);
       });
     }
   }
-  };
+};
--- server/schema.sql
+++ server/schema.sql
@@ -15,6 +15,9 @@ CREATE TABLE teams (
   id int NOT NULL AUTO_INCREMENT,
   teamname varchar(200)  NOT NULL,
   rank int NOT NULL,
+  wins int NOT NULL,
+  losses int NOT NULL,
+  ties int NOT NULL,
   league int NOT NULL,
   PRIMARY KEY (ID)
 );
@@ -22,6 +25,17 @@ CREATE TABLE teams (
 CREATE TABLE users (
   id        int    NOT NULL AUTO_INCREMENT,
   username  varchar(40)   NOT NULL,
+  PRIMARY KEY (ID)
+);
+
+
+CREATE TABLE games (
+  id        int    NOT NULL AUTO_INCREMENT,
+  team1  int   NOT NULL,
+  team2  int   NOT NULL,
+  team1score  int   NOT NULL,
+  team2score  int   NOT NULL,
+  date DATE NOT NULL,
   PRIMARY KEY (ID)
 );
 
