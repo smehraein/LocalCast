c2c5c8b8179ad8c432555d3f8b809c4a2a1b60d5
Soroush Mehraein <soroushmehraein@gmail.com>
2015-11-09 12:28:57 -0800

    Switch to ORM model
    Forgot about this - would rather do this sooner than later

--- server/db/index.js
+++ server/db/index.js
@@ -1,12 +1,51 @@
-var mysql = require('mysql');
+var Sequelize = require("sequelize");
+var orm = new Sequelize("localCast", "root", "SQL");
 
 
-var connection = mysql.createConnection({
-  user: "root",
-  password: "SQL",
-  database: "localCast"
+// we define the models we need using js--we don't need a schema file!
+var User = orm.define('User', {
+  username: Sequelize.STRING
 });
 
-connection.connect();
+var League = orm.define('League', {
+  leaguename: Sequelize.STRING,
+  sport: Sequelize.STRING
+});
 
-module.exports = connection;
+var Team = orm.define('Team', {
+  teamname: Sequelize.STRING,
+  rank: Sequelize.INTEGER,
+  losses: Sequelize.INTEGER,
+  wins: Sequelize.INTEGER,
+  ties: Sequelize.INTEGER,
+});
+
+var Game = orm.define('Game', {
+  team1: Sequelize.INTEGER,
+  team2: Sequelize.INTEGER,
+  team1score: Sequelize.INTEGER,
+  team2score: Sequelize.INTEGER,
+  date: Sequelize.DATE,
+});
+
+
+
+Team.belongsTo(League);
+User.belongsTo(Team);
+League.hasMany(Team);
+Team.hasMany(Game);
+Team.hasMany(User);
+Game.hasMany(Team);
+
+
+User.sync();
+League.sync();
+Team.sync();
+Game.sync();
+// creates these tables in MySQL if they don't already exist. Pass in {force: true}
+// to drop any existing user and message tables and make new ones.
+
+exports.User = User;
+exports.League = League;
+exports.Team = Team;
+exports.Team = Team;
